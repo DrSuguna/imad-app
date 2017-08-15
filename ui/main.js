e2a063549d3d667ebf3f22997d86088c84f12731
DrSuguna <30868136+DrSuguna@users.noreply.github.com>
2017-08-15 21:19:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='You have changed the text';
+var img=document.getElementById('imad');
+img.onclick=function()
+{ img.style.marginLeft='100px';
+}
