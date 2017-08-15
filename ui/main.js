e1748ff9fdae23f54464ae9eee7643047b9093e3
DrSuguna <30868136+DrSuguna@users.noreply.github.com>
2017-08-15 21:14:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text')
-element.innerHtml='You have changed the text'
+var element=document.getElementById('main-text');
+element.innerHtml='You have changed the text';
