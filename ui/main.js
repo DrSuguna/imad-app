11d0b6a9fcedcedc49937de87bccb599ebe25403
DrSuguna <30868136+DrSuguna@users.noreply.github.com>
2017-08-15 21:24:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='You have changed the text';
 var img=document.getElementById('imad');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function()
-{ img.style.marginLeft='100px';
+{ 
+    var interval = setInterval(moveRight, 100);
 }
